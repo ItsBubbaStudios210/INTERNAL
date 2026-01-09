c99de588b1ba26916ce34026383a119f95c34b75
ItsBubbaStudiosOffical <itsbubbastudios3@gmail.com>
2026-01-09 23:05:51 +0000

    Added a boot function and one thing to the idea list

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+import time
 #Varibiles
 Name, Version, Creator = "INTERNAL", "BETA", "ItsBubbaStudiosOffical"
 
 # Idea's List
 ideas_list = {
     "IDEA TEMPLATE ('Idea' made by 'Creator' its was on 'Platform')",
-    "INTERNAL frist brought from ItsBubbaStudiosOffical"
+    "INTERNAL frist brought from ItsBubbaStudiosOffical",
+    "Word Maker by My sibiling"
 }
 
 # Print All Of The Idea's
 for item in ideas_list:
     print(item)
+
+def boot(MaxPercentage, Process, command, Delay):
+    percenage = 0
+    for i in range(MaxPercentage):
+        percenage += 1
+        print(f"Process: {Process}\n{percenage} / {MaxPercentage}")
+        time.sleep(Delay)
+    command()
+boot(200, "Booting", None, 0.1)
