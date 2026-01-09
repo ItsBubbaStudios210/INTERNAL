eca3c0722ad1ae87654786dd32d90b7ab29454ff
ItsBubbaStudiosOffical <itsbubbastudios3@gmail.com>
2026-01-09 11:41:10 +0000

    Added some varibiles

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+#Varibiles
+Name, Version, Creator = "INTERNAL", "BETA", "ItsBubbaStudiosOffical"
+
+# Idea's List
 ideas_list = {
-    "IDEA TEMPLATE ('Idea' made by 'Creator' its was on 'Platform')"
+    "IDEA TEMPLATE ('Idea' made by 'Creator' its was on 'Platform')",
+    "INTERNAL frist brought from ItsBubbaStudiosOffical"
 }
 
+# Print All Of The Idea's
 for item in ideas_list:
     print(item)
